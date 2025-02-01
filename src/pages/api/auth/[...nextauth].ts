716efe81744dce17599970372041f24b435a190c
felipedev <felipedequadrosgomes18@gmail.com>
2025-02-01 20:32:57 -0300

    atualizando

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,7 +10,7 @@ export const authOptions = {
     }),
   
   ],
-  secret: `${process.env.JWT_SECRET}`as string,
+  secret: `${process.env.NEXTAUTH_SECRET}`as string,
 }
 
 export default NextAuth(authOptions)
