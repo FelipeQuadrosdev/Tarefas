a90659aa5633b31f9d15c653f894d1c8269549d2
felipedev <felipedequadrosgomes18@gmail.com>
2025-02-01 20:36:17 -0300

    atualizando envi

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,7 +10,7 @@ export const authOptions = {
     }),
   
   ],
-  secret: `${process.env.NEXTAUTH_SECRET}`as string,
+  secret: `${process.env.NEXTAUTH_SECRE}`as string,
 }
 
 export default NextAuth(authOptions)
