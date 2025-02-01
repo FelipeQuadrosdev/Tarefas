ead1021f7b1d27363d1fc0cede1297b3c5b1dcc7
felipedev <felipedequadrosgomes18@gmail.com>
2025-02-01 20:26:40 -0300

    atualizando o env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
+import { SessionProvider } from "next-auth/react"
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Header } from "../components/header/index";
-import { SessionProvider } from "next-auth/react"
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
