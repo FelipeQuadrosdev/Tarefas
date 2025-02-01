f67e45579867527d1a2de50c9a24f160d2fe4329
felipedev <felipedequadrosgomes18@gmail.com>
2025-02-01 20:47:55 -0300

    atualizando

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,9 +8,8 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-  
   ],
   secret: process.env.NEXTAUTH_SECRET as string,
-}
+};
 
 export default NextAuth(authOptions);
